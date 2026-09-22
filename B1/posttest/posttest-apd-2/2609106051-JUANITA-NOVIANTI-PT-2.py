20812861aa6e024bb834d76fd8f32996713a1078
juanitanovianti <juanitaaanovianti>
2026-09-22 22:27:44 +0800

    Melakukan Pengecekan Hasil Print

---
+++ B1/posttest/posttest-apd-2/2609106051-JUANITA-NOVIANTI-PT-2.py
@@ -0,0 +1,49 @@
+makanan_1 = 15000
+makanan_2 = 16000
+makanan_3 = 19000
+makanan_4 = 20000
+makanan_5 = 21000
+makanan_6 = 22000
+
+print(makanan_1, makanan_2, makanan_3, makanan_4, makanan_5, makanan_6)
+
+biaya_aplikasi = 5000
+
+print(biaya_aplikasi)
+
+harga_makanan = [15000, 16000, 19000, 20000, 21000, 22000]
+
+print(harga_makanan[0:7])
+print(harga_makanan[-7:])
+
+total_bayar = 15000 + 16000 + 19000 + 20000 + 21000 + 22000 + 5000
+
+print(total_bayar)
+
+banyak_data = [makanan_1, makanan_2, makanan_3, makanan_4, makanan_5, makanan_6, biaya_aplikasi]
+
+print(banyak_data)
+
+rata_rata = total_bayar / (len(banyak_data))
+
+print(rata_rata)
+
+nim = 51
+
+print(nim)
+
+bolean = nim != rata_rata
+
+print(bolean)
+
+konversi_EUR = 5.77
+
+print(konversi_EUR)
+
+
+
+
+
+
+
+
